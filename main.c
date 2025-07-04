59c445ad3f58a876de67f77927790350343ecdfe
aaronprais <prais.aaron@gmail.com>
2025-07-04 11:51:58 +0300

    first files

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_FILENAME 100
+
+int main(int argc, char *argv[]) {
+    FILE *fp;
+    char filename[MAX_FILENAME];
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <file1> [file2] [file3] ...\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        snprintf(filename, MAX_FILENAME, "%s.as", argv[i]);
+        fp = fopen(filename, "r");
+        if (fp == NULL) {
+            fprintf(stderr, "Error: cannot open %s\n", filename);
+            continue; // process next file
+        }
+
+        printf("Processing file: %s\n", filename);
+        
+        fclose(fp);
+    }
+
+    return EXIT_SUCCESS;
+}
